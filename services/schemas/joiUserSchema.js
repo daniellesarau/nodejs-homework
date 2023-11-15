3032a63ad6f533a28657e73e8054459d8f60ac19
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2023-11-15 17:09:02 +0200

    Update joiUserSchema.js

--- services/schemas/joiUserSchema.js
+++ services/schemas/joiUserSchema.js
@@ -1,6 +1,5 @@
 const Joi = require("joi");
 
-// SCHEMA \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const schemaRegisterUser = Joi.object({
  email: Joi.string()
   .email({
@@ -14,7 +13,6 @@ const schemaRegisterUser = Joi.object({
  subscription: Joi.any().valid("starter", "pro", "business").default("starter"),
 });
 
-// VALIDATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const checkError = (schema, { body }, res, next) => {
  const { error } = schema.validate(body);
 
